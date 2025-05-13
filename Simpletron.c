7acd1652ee5013085c5f9dd411608f6f0c333714
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-05-13 09:44:21 -0300

    feat: add opcodes definitions

---
+++ Simpletron.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//Operações Entrada/Saida
+#define READ 10 //coloca a plaavra na memória
+#define WRITE 11 //escreve uma palavra da memória no terminal
+
+//Operações de Carregamento/ARmazenamento
+#define LOAD 20 //
+#define STORE 21
+
+//Operações Aritméticas
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+
+//Operações de Transferência de Controle
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
